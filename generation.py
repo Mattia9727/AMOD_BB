402f203f49f7a031037b12a5133d08831d2b3bec
Mattia971DJ <mattiaantonageli@gmail.com>
2022-07-01 18:14:45 +0200

    correzione generazione e parsing liste di tempi e precedenze

--- generation.py
+++ generation.py
@@ -21,20 +21,34 @@ def generation(N,n,k):
 
         with open('instances.txt', 'a') as file:
             for j in range(n):
-                file.write(str(Pi[j]) + "\t");
+                file.write(str(Pi[j]) + "_");
             file.write("\n")
             for p in precs:
-                file.write(str(p[0]) + "," + str(p[1]) + "\t")
+                file.write(str(p[0]) + "," + str(p[1]) + "_")
             file.write("\n\n")
         file.close()
 
     return
 
 def parse_prec(p):
-    prec_list = p.split("\t")
-    for i in prec_list:
-        prec_list.append(i[0].split(","))
-    return prec_list
+    if p[-1:]=="\n":
+        p=p[:-1]
+    p_list = p.split("_")
+    ret_list=[]
+    for i in p_list:
+        ret_list.append(i.split(","))
+        for j in ret_list:
+            j[0]=int(j[0])
+            j[1]=int(j[1])
+    return ret_list
+
+def parse_pr_time(p):
+    if p[-1:]=="\n":
+        p=p[:-1]
+    p_list = p.split("_")
+    map_object = map(int, p_list)
+    ret_list = list(map_object)
+    return ret_list
 
 
 
